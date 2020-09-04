6df16ca61f2ed9fa7d3d810f6c87d256961ba6b9
Julian Palladino <julianpalladino8@gmail.com>
2020-09-04 15:04:57 -0700

    Adds constants file

---
+++ analysis/constants.py
@@ -0,0 +1,5 @@
+PANDAS_CHUNKSIZE = 1000
+DEFAULT_EDGES_FILE_PATH = "../LivingCity/berkeley_2018/new_full_network/edges.csv"
+DEFAULT_DISTANCE_MERGE_OUTPUT_FILE_PATH = "distances.csv"
+DEFAULT_PEOPLE_FILE_PATH = "../LivingCity/0_people5to12.csv"
+DEFAULT_ROUTE_FILE_PATH = "../LivingCity/0_route5to12.csv"
